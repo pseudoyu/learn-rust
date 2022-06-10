2cf2f8b2cd9601348b4d9b9a1eed257728a21873
pseudoyu <pseudoyu@connect.hku.hk>
2022-06-10 22:27:19 +0800

    feat: add errors in scrape_url

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,15 +1,35 @@
 use std::fs;
 
-fn main() {
+// fn main() {
+//     let url = "https://www.rust-lang.org/";
+//     let output = "rust.md";
+
+//     print!("Fetching url {}...", url);
+//     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+//     println!("Converting html to markdown...");
+//     let md = html2md::parse_html(&body);
+
+//     fs::write(output, md.as_bytes()).unwrap();
+//     println!("Converted markdown has been saved in {}", output);
+// }
+
+// main() 返回 Result<T,E>
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.rust-lang.org/";
     let output = "rust.md";
 
     print!("Fetching url {}...", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    // 使用 ? 传播错误
+    let body = reqwest::blocking::get(url).unwrap().text()?;
 
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    fs::write(output, md.as_bytes())?;
     println!("Converted markdown has been saved in {}", output);
+
+    // 返回 T 正确类型
+    Ok(())
 }
